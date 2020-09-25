935a5e082acf540902a6df2f592d60fc6a466b1b
Michael Dockter <michael@dockter.com>
2020-09-25 10:44:44 -0400

    #149 Prepare for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,13 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 [markdownlint](https://dlaa.me/markdownlint/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.6.3] - 2020-09-25
+
+### Added in 1.6.3
+
+- Support for stack traces using `gdb`.
+- Support for `SENZING_RABBITMQ_HEARTBEAT_IN_SECONDS`
+
 ## [1.6.2] - 2020-09-21
 
 ### Changed in 1.6.2
--- stream-loader.py
+++ stream-loader.py
@@ -40,9 +40,9 @@ except ImportError:
     pass
 
 __all__ = []
-__version__ = "1.6.2"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.6.3"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2018-10-29'
-__updated__ = '2020-09-24'
+__updated__ = '2020-09-25'
 
 SENZING_PRODUCT_ID = "5001"  # See https://github.com/Senzing/knowledge-base/blob/master/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'

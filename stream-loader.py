6a90ee234e78419d4a1af2d01b8031e376230690
michael@dockter.com <michael@dockter.com>
2021-06-30 10:24:54 -0400

    #220 Successfully tested WithInfo

--- stream-loader.py
+++ stream-loader.py
@@ -43,9 +43,9 @@ except ImportError:
     pass
 
 __all__ = []
-__version__ = "1.7.4"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.8.0"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2018-10-29'
-__updated__ = '2021-06-29'
+__updated__ = '2021-06-30'
 
 SENZING_PRODUCT_ID = "5001"  # See https://github.com/Senzing/knowledge-base/blob/master/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'

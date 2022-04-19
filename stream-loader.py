2703ac622309869f9f858ea1c9f6d65479728c40
docktermj <michael@dockter.com>
2022-04-19 10:42:42 -0400

    #291 Prepare for versioning

--- stream-loader.py
+++ stream-loader.py
@@ -64,7 +64,7 @@ except:
 # Metadata
 
 __all__ = []
-__version__ = "1.10.0"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.10.1"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2018-10-29'
 __updated__ = '2022-04-19'
 

a43005e02c1760e9b07e6432cabdd16fcdba58da
docktermj <michael@dockter.com>
2022-06-30 08:07:17 -0400

    #311 Savepoint.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 [markdownlint](https://dlaa.me/markdownlint/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.10.7] - 2022-06-29
+## [1.10.7] - 2022-06-30
 
 ### Changed in 1.10.7
 
--- stream-loader.py
+++ stream-loader.py
@@ -67,7 +67,7 @@ except Exception:
 __all__ = []
 __version__ = "1.10.7"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2018-10-29'
-__updated__ = '2022-06-29'
+__updated__ = '2022-06-30'
 
 SENZING_PRODUCT_ID = "5001"  # See https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'
